a02f19b4288cc27462df1dd65a8dd8a6cdbbdc0b
Seok-CH <tjr6677@gmail.com>
2023-02-25 21:40:27 +0900

    [#1] chore: 타입스크립트 관련 패키지 다운 및 config 설정

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,13 @@ module.exports = {
     },
   },
   plugins: [
+    {
+      resolve: "gatsby-plugin-typescript",
+      options: {
+        isTSX: true,
+        allExtensions: true,
+      },
+    },
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
