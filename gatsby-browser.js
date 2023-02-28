4bb7a4348e2b13c6edc3be00ded19a6f131315e2
Seok-CH <tjr6677@gmail.com>
2023-02-28 20:49:41 +0900

    Design: style.css 파일을 디자인 토큰과 global.css로 분리, 다크모드를 위해 theme.css 추가
    - color token은 tailwindcss에서 가져왔음
    - 나머지 token은 기본 세팅으로 나뒀음
    - style.css는 현재 특정 element의 스타일링에 대한 코드가 있는데 추후 변경될 예정
    - theme.css는 html element의 data-* 속성을 이용하여 정할 예정

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,11 @@
 // custom CSS styles
+import './src/styles/base/_color.css';
+import './src/styles/base/_layout.css';
+import './src/styles/base/_spacing.css';
+import './src/styles/base/_typography.css';
+import './src/styles/global.css';
 import './src/styles/style.css';
+import './src/styles/theme.css';
 
 // Highlighting for code blocks
 import 'prismjs/themes/prism.css';
