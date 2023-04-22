76e7f77eccf0dd7ea3c94fc05b9eb7172f736ae9
chadseok <tjr6677@gmail.com>
2023-04-22 22:21:39 +0900

    [#2] fix: blog list 파일에서 description 안나오는 버그 수정
    블로그 내용의 첫부분을 잘라서 보여주는 기능이 제대로 작동을 하지않아 (truncate: true)옵션을 주었습니다

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -78,7 +78,7 @@ export const pageQuery = graphql`
     }
     allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
       nodes {
-        excerpt
+        excerpt(truncate: true)
         fields {
           slug
         }
