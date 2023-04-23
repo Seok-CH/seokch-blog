b38ed4917071930bc5b3d3f6f78d95e2c51c7cc4
chadseok <tjr6677@gmail.com>
2023-04-23 15:12:34 +0900

    [#2] design: 이미지 파일 최대 크기 1024px로 변경
    블로그 포스트의 이미지들이 width를 꽉채우지 못하는 것 같아 최대 크기를 1024px로 변경하였습니다

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,7 +59,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 630,
+              maxWidth: 1024,
             },
           },
           {
