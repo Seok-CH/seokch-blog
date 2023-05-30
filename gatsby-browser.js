291d8dd5c7d6022ed1c3809b18d99e00b30f61b3
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-05-30 18:19:02 +0900

    [#2] refactor: style.css에서 통합적으로 관리하던 컴포넌트 스타일링 분리

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,8 +5,11 @@ import './src/styles/base/_layout.css';
 import './src/styles/base/_spacing.css';
 import './src/styles/base/_typography.css';
 import './src/styles/global.css';
-import './src/styles/style.css';
 import './src/styles/theme.css';
+// component styles
+import './src/styles/components/layout.css';
+import './src/styles/components/blog.css';
+import './src/styles/components/post.css';
 
 // Highlighting for code blocks
 import 'prism-material-themes/themes/material-default.css';
