84202e61c44348c256b1b1065ee1154f35e378d2
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-05-31 18:26:36 +0900

    [#2] move: 각종 글로벌 스타일 파일을 global폴더에 묶음

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,13 @@
-// custom CSS styles
-import './src/styles/normalize.css';
+// Base CSS token
 import './src/styles/base/_color.css';
 import './src/styles/base/_layout.css';
 import './src/styles/base/_spacing.css';
 import './src/styles/base/_typography.css';
-import './src/styles/global.css';
-import './src/styles/theme.css';
-// component styles
+// Global CSS Styles
+import './src/styles/global/frame.css';
+import './src/styles/global/normalize.css';
+import './src/styles/global/theme.css';
+// Component CSS styles
 import './src/styles/components/layout.css';
 import './src/styles/components/blog.css';
 import './src/styles/components/post.css';
